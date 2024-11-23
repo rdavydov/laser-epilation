fd7b35f1cc7809ed07e7dfd87e02dd24e2a74be8
Roman Davydov <15850461+rdavydov@users.noreply.github.com>
2024-11-23 15:41:02 +0300

    из Мытищ в Волгодонск

--- assets/js/viewport.js
+++ assets/js/viewport.js
@@ -2,6 +2,6 @@ const address = document.querySelector(".header_address");
 const types = document.querySelector('.soprano_regular__types')
 
 if (window.matchMedia("(max-width: 425px)").matches) {
-    address.innerText = "мытищи, ул. мира, 35";
+    address.innerText = "Волгодонск, ул. Кошевого, 27";
     types.innerText = "Подходит для всех типов кожи и волос";
 }
